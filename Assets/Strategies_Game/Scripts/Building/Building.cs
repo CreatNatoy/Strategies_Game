f4675cafa0f5ea79f72337c4be999006d63883b1
Anton Pashkevich <natoy7000@gmail.com>
2022-09-21 15:38:34 +0200

    Building (Colors)

--- Assets/Strategies_Game/Scripts/Building/Building.cs
+++ Assets/Strategies_Game/Scripts/Building/Building.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Building : SelectableObject
@@ -5,9 +6,17 @@ public class Building : SelectableObject
     [SerializeField] private int _price;
     [SerializeField] private int _xSize = 3;
     [SerializeField] private int _zSize = 3;
-
-    public int Price => _price; 
+    [SerializeField] private Renderer _renderer; 
     
+    private Color _startColor;
+    public int XSize => _xSize;
+    public int ZSize => _zSize;
+    public int Price => _price;
+
+    private void Awake() {
+        _startColor = _renderer.material.color;
+    }
+
     private void OnDrawGizmos() {
         var cellSize = BuildingPlacer.CellSize; 
         for (var x = 0; x < _xSize; x++) {
@@ -16,5 +25,13 @@ public class Building : SelectableObject
                 Gizmos.DrawWireCube(transform.position + new Vector3(x, 0, z) * cellSize, new Vector3(1,0,1) * cellSize);
             }
         }
+    }
+
+    public void DisplayUnacceptablePosition() {
+        _renderer.material.color = Color.red; 
+    }
+
+    public void DisplayAcceptablePosition() {
+        _renderer.material.color = _startColor; 
     }
 }
--- Assets/Strategies_Game/Scripts/Building/BuildingPlacer.cs
+++ Assets/Strategies_Game/Scripts/Building/BuildingPlacer.cs
@@ -1,19 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildingPlacer : MonoBehaviour
 {
     [SerializeField] private Camera _raycastCamera;
     [SerializeField] private float _cellSize = 1f;
-    
+
     private Plane _plane;
-   [SerializeField] private Building _currentBuilding;
-   private ServiceLocator _serviceLocator; 
-   
+    private Dictionary<Vector2Int, Building> _buildingsDictionary = new Dictionary<Vector2Int, Building>(); 
+    private Building _currentBuilding;
+    private ServiceLocator _serviceLocator;
+
     public static float CellSize = 1f;
 
     private void Awake() {
-        CellSize = _cellSize; 
-        
+        CellSize = _cellSize;
+
         _serviceLocator = ServiceLocator.Instance;
         _serviceLocator.Register(this);
     }
@@ -23,34 +25,62 @@ public class BuildingPlacer : MonoBehaviour
     }
 
     private void Update() {
-        if(_currentBuilding == null) return; 
-        
-        _currentBuilding.transform.position = GetPointRaycast();
+        if (_currentBuilding == null) return;
 
-        if (Input.GetMouseButtonDown(0)) {
-            _currentBuilding = null; 
+        _currentBuilding.transform.position = GetPointRaycast(out var x, out var z);
+
+        if (CheckAllow(x, z)) {
+            _currentBuilding.DisplayAcceptablePosition();
+            
+            if (Input.GetMouseButtonDown(0)) {
+                InstallBuilding(x,z);
+                _currentBuilding = null;
+            }
+        }
+        else {
+            _currentBuilding.DisplayUnacceptablePosition();
+        }
+        
+    }
+
+    private bool CheckAllow(int xPosition, int zPosition) {
+        for (var x = 0; x < _currentBuilding.XSize; x++) {
+            for (var z = 0; z < _currentBuilding.ZSize; z++) {
+                var coordinate = new Vector2Int(xPosition + x, zPosition + z);
+                if (_buildingsDictionary.ContainsKey(coordinate)) return false; 
+            }
+        }
+        return true;
+    }
+
+    private void InstallBuilding(int xPosition, int zPosition) {
+        for (var x = 0; x < _currentBuilding.XSize; x++) {
+            for (var z = 0; z < _currentBuilding.ZSize; z++) {
+                var coordinate = new Vector2Int(xPosition + x, zPosition + z); 
+                _buildingsDictionary.Add(coordinate, _currentBuilding);
+            }
         }
     }
 
-    private Vector3 GetPointRaycast() {
+    private Vector3 GetPointRaycast(out int x, out int z) {
         var ray = _raycastCamera.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(ray.origin, ray.direction * 10f, Color.green);
 
         _plane.Raycast(ray, out var distance);
         var point = ray.GetPoint(distance) / _cellSize;
-        
-        var x = Mathf.RoundToInt(point.x);
-        var z = Mathf.RoundToInt(point.z); 
-        
-        return new Vector3(x,0,z) * _cellSize;
+
+        x = Mathf.RoundToInt(point.x);
+        z = Mathf.RoundToInt(point.z);
+
+        return new Vector3(x, 0, z) * _cellSize;
     }
-    
+
     private void OnValidate() {
         CellSize = _cellSize;
     }
 
     public void CreateBuilding(Building buildingPrefab) {
-       var newBuilding = Instantiate(buildingPrefab);
-       _currentBuilding = newBuilding;
+        var newBuilding = Instantiate(buildingPrefab);
+        _currentBuilding = newBuilding;
     }
 }
